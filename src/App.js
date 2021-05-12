847f320c05c4378ea02fa2ded5c0d26d165027a5
7204-OMER <Eyupbhr24@gmail.com>
2021-05-12 12:44:10 +0300

    changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import email from "./assets/email.svg";
+// import location from "./location.svg";
+// import phone from "./phone.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo">
+        <img src={email} className="App-logo" alt="logo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
