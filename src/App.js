9238a998ced6d3170c9093ba7d055c4a337d3020
7204-OMER <Eyupbhr24@gmail.com>
2021-05-12 13:21:32 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import email from "./assets/email.svg";
-// import location from "./location.svg";
-// import phone from "./phone.svg";
+import location from "./assets/location.svg";
+import phone from "./assets/phone.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="pic">{/* <img src= {} alt="" /> */}</div>
       <div className="logo">
         <img src={email} className="App-logo" alt="logo" />
+      </div>
+      <div>
+        <p>Email adress</p>
+      </div>
+      <div className="logo">
+        <img src={location} className="App-logo" alt="logo" />
+      </div>
+      <div>
+        <p>Location</p>
+      </div>
+      <div className="logo">
+        <img src={phone} className="App-logo" alt="logo" />
+      </div>
+      <div>
+        <p>phone number</p>
       </div>
     </div>
   );
